49186c277927a9b61b003f2c145889b2f9e8ce05
aclgo <marcellosanttos2014@gmail.com>
2023-09-23 10:21:11 -0300

    create models to conteins object type of mail body and function of validate data

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,11 +1,16 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type MailBody struct {
-	From     string
-	To       string
-	Subject  string
-	Body     string
-	Template string
+	From     string `json:"from"`
+	To       string `json:"to"`
+	Subject  string `json:"subject"`
+	Body     string `json:"body"`
+	Template string `json:"template"`
 }
 
 func NewMailBody(from string, to string, subject, body, template string) *MailBody {
@@ -16,4 +21,31 @@ func NewMailBody(from string, to string, subject, body, template string) *MailBo
 		Body:     body,
 		Template: template,
 	}
+}
+
+func (m *MailBody) Validate() error {
+
+	f := func(arg string) string {
+		return fmt.Sprintf("empty %s", arg)
+	}
+
+	if m.From == "" {
+		return errors.New(f("from"))
+	}
+
+	if m.To == "" {
+		return errors.New(f("to"))
+	}
+	if m.Subject == "" {
+		return errors.New(f("subject"))
+	}
+	if m.Body == "" {
+		return errors.New(f("body"))
+	}
+
+	if m.Template == "" {
+		return nil
+	}
+
+	return nil
 }
