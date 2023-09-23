1269f76c9a32ba921f90e023b63cd7f611b1fd5e
aclgo <marcellosanttos2014@gmail.com>
2023-09-23 10:17:01 -0300

    create basic handlers to receive data grpc and validate, if all data ok sending mail

--- internal/mail/delivery/grpc/service/handlers.go
+++ internal/mail/delivery/grpc/service/handlers.go
@@ -1,1 +1,28 @@
 package service
+
+import (
+	"context"
+
+	"github.com/aclgo/grpc-mail/internal/mail"
+	"github.com/aclgo/grpc-mail/internal/models"
+	"github.com/aclgo/grpc-mail/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *MailService) SendService(ctx context.Context, req *proto.MailRequest) (*proto.MailResponse, error) {
+	data := models.NewMailBody(req.From, req.To, req.Subject, req.Body, "")
+
+	if err := data.Validate(); err != nil {
+		s.logger.Errorf("SendService.Validate: %v", err)
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	err := s.mailUC.Send(data)
+	if err != nil {
+		s.logger.Errorf("SendService.Send: %v", err)
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &proto.MailResponse{Message: mail.EmailSentSuccess}, nil
+}
--- internal/mail/delivery/grpc/service/service.go
+++ internal/mail/delivery/grpc/service/service.go
@@ -1,3 +1,20 @@
 package service
 
-type MailService struct{}
+import (
+	"github.com/aclgo/grpc-mail/internal/mail"
+	"github.com/aclgo/grpc-mail/pkg/logger"
+	"github.com/aclgo/grpc-mail/proto"
+)
+
+type MailService struct {
+	mailUC mail.MailUseCase
+	logger logger.Logger
+	proto.UnimplementedMailServiceServer
+}
+
+func NewMailService(mailUC mail.MailUseCase, logger logger.Logger) *MailService {
+	return &MailService{
+		mailUC: mailUC,
+		logger: logger,
+	}
+}
