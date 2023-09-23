4b3d958325369f5955b0b15befaf97af8f49a1ac
aclgo <marcellosanttos2014@gmail.com>
2023-09-23 10:25:51 -0300

    basic config temporary to running application

--- cmd/grpc-mail/main.go
+++ cmd/grpc-mail/main.go
@@ -1,35 +1,58 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/aclgo/grpc-mail/config"
-	"github.com/aclgo/grpc-mail/internal/gmail"
-	"github.com/aclgo/grpc-mail/internal/models"
-	"github.com/aclgo/grpc-mail/internal/ses"
-	"github.com/aclgo/grpc-mail/pkg/redis"
+	"github.com/aclgo/grpc-mail/internal/adapters/gmail"
+	"github.com/aclgo/grpc-mail/internal/adapters/ses"
+	grpcService "github.com/aclgo/grpc-mail/internal/mail/delivery/grpc/service"
+	httpService "github.com/aclgo/grpc-mail/internal/mail/delivery/http/service"
+	"github.com/aclgo/grpc-mail/internal/mail/usecase"
+	"github.com/aclgo/grpc-mail/internal/server"
+	"github.com/aclgo/grpc-mail/pkg/logger"
 )
 
 func main() {
 
-	cfg := config.NewConfig()
+	cfg := config.Load(".")
+	cfg.ServiceHTTPPort = 3000
 
-	redisClient := redis.Connect(cfg)
-
-	_ = redisClient
+	logger := logger.NewapiLogger(cfg)
 
 	ses := ses.NewSes(cfg)
 	gmail := gmail.NewGmail(cfg)
 
-	data := models.NewMailBody("from", "to", "subject", "body", "template")
+	sesUC := usecase.NewmailUseCase(ses, logger)
+	gmailUC := usecase.NewmailUseCase(gmail, logger)
 
-	err := ses.Send(data)
-	if err != nil {
-		log.Println(err)
-	}
+	serviceSesHTTP := httpService.NewMailService(sesUC, logger)
+	serviceSesGRPC := grpcService.NewMailService(sesUC, logger)
 
-	err = gmail.Send(data)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	serviceGmailHTTP := httpService.NewMailService(gmailUC, logger)
+	serviceGmailGRPC := grpcService.NewMailService(gmailUC, logger)
+
+	handlerSvcSes := server.NewHttpHandlerService("/ses", serviceSesHTTP)
+	handlerSvcGmail := server.NewHttpHandlerService("/gmail", serviceGmailHTTP)
+
+	server := server.NewServer(cfg,
+		logger,
+		[]*server.HttpHandlerService{
+			handlerSvcSes,
+			handlerSvcGmail,
+		},
+		[]*grpcService.MailService{
+			serviceSesGRPC,
+			serviceGmailGRPC,
+		},
+	)
+
+	signal, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	defer stop()
+
+	log.Fatal(server.Run(signal))
 }
