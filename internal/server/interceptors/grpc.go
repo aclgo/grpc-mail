40e714a6451f6f0b727de63bb14953e3daa2785a
aclgo <marcellosanttos2014@gmail.com>
2023-09-26 21:42:02 -0300

    create intercept to logging grpc transport

--- internal/server/interceptors/grpc.go
+++ internal/server/interceptors/grpc.go
@@ -1,1 +1,33 @@
 package interceptors
+
+import (
+	"context"
+	"time"
+
+	"github.com/aclgo/grpc-mail/pkg/logger"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+type interceptorGRPC struct {
+	logger logger.Logger
+}
+
+func NewinterceptorGRPC(logger logger.Logger) *interceptorGRPC {
+	return &interceptorGRPC{
+		logger: logger,
+	}
+}
+
+func (i *interceptorGRPC) Logger(ctx context.Context,
+	req any, info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (resp any, err error) {
+	start := time.Now()
+
+	md, _ := metadata.FromIncomingContext(ctx)
+	reply, err := handler(ctx, req)
+
+	i.logger.Infof("METADATA: %v, METHOD: %v, TIME: %v, ERR: %v", md, info.FullMethod, time.Since(start), err)
+
+	return reply, err
+}
