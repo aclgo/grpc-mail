b34c8f461a5ecd74ce373b113381d531851dc180
aclgo <marcellosanttos2014@gmail.com>
2023-10-29 17:16:00 -0300

    added basic e2e test for testing handlers http and grpx

---
+++ e2e/e2eGRPC.go
@@ -0,0 +1,69 @@
+package e2e
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/aclgo/grpc-mail/internal/mail"
+	"github.com/aclgo/grpc-mail/internal/models"
+	"github.com/aclgo/grpc-mail/pkg/logger"
+	"github.com/aclgo/grpc-mail/proto"
+	"google.golang.org/grpc"
+)
+
+type e2eGRPCTest struct {
+	mailClient proto.MailServiceClient
+}
+
+func Newe2eClient(mailClient proto.MailServiceClient) *e2eGRPCTest {
+	return &e2eGRPCTest{
+		mailClient: mailClient,
+	}
+}
+
+func RunGRPC(addrServer string, logger logger.Logger) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(10)*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(ctx, addrServer, grpc.WithInsecure())
+	if err != nil {
+		logger.Errorf("Run.DialContext: %v", err)
+		return
+	}
+
+	mailClient := proto.NewMailServiceClient(conn)
+
+	param := models.NewMailBody(
+		"from",
+		"to",
+		"subject",
+		"body",
+		"<h1>hello</h1>",
+		"service name",
+	)
+
+	_, err = mailClient.SendService(
+		ctx,
+		&proto.MailRequest{
+			From:        param.From,
+			To:          param.To,
+			Subject:     param.Subject,
+			Body:        param.Body,
+			Template:    param.Template,
+			Servicename: param.ServiceName,
+		},
+	)
+
+	if err != nil {
+		if strings.Contains(err.Error(), mail.ErrServiceNameNotExist.Error()) {
+			logger.Info("TEST e2e GRPC PASS")
+			return
+		}
+
+		logger.Errorf("Run.SendService: %v", err)
+	}
+
+	logger.Info("TEST e2e GRPC PASS")
+}
---
+++ e2e/e2eHTTP.go
@@ -0,0 +1,63 @@
+package e2e
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/aclgo/grpc-mail/internal/models"
+	"github.com/aclgo/grpc-mail/pkg/logger"
+)
+
+type e2eHTTPTest struct {
+}
+
+func Newe2eTest() *e2eHTTPTest {
+	return &e2eHTTPTest{}
+}
+
+func RunHTTP(addrServer string, logger logger.Logger) {
+	params := models.NewMailBody(
+		"from",
+		"to",
+		"subject",
+		"body",
+		"template",
+		"service_name",
+	)
+
+	js, err := json.Marshal(params)
+	if err != nil {
+		logger.Errorf("Run.Marshal: %v", err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "POST", addrServer, bytes.NewReader(js))
+	if err != nil {
+		logger.Errorf("Run.NewRequestWithContext: %v", err)
+		return
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		logger.Errorf("Run.Default.Client.Do: %v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusBadRequest || resp.StatusCode != http.StatusOK {
+		logger.Errorf("ERROR e2e test http endpoint")
+		return
+	}
+
+	logger.Info("TEST e2e HTTP PASS")
+
+}
